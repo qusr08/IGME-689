9721fdb49c70a8c2c94ec3713a3f9ca1266ace13
Frank Alfano <q24xboss@gmail.com>
2025-09-07 22:38:25 -0400

    Math updates for sun positioning

--- Assignment 1/Assets/Scripts/RealtimeSunController.cs
+++ Assignment 1/Assets/Scripts/RealtimeSunController.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class RealtimeSunController : MonoBehaviour
 {
 	[SerializeField] private MapLocationController mapLocationController;
+	[SerializeField] private ArcGISCameraComponent arcGISCameraController;
 	[SerializeField] private ArcGISMapComponent arcGISMapComponent;
+
+	private float earthCircumferenceMeters = 40075f;
+	private float earthAxialTiltRadians = 23.5f * Mathf.Deg2Rad;
 
 	private void Update ( )
 	{
-		// Get the current UTC time
-		DateTime currentLocationTime = DateTime.UtcNow;
+		// Get the current UTC time and map location
+		DateTime now = DateTime.UtcNow;
+		Vector2 currentLocationCoordinates = mapLocationController.Locations[mapLocationController.CurrentLocationIndex];
 
 		// Calculate the number of seconds that have elapsed within the day
 		float currentDaySeconds = 0;
-		currentDaySeconds += currentLocationTime.Hour * 3600f;
-		currentDaySeconds += currentLocationTime.Minute * 60f;
-		currentDaySeconds += currentLocationTime.Second;
+		currentDaySeconds += now.Hour * 3600f;
+		currentDaySeconds += now.Minute * 60f;
+		currentDaySeconds += now.Second;
 
 		// Calculate the current longitude of the sun
 		float sunLongitude = currentDaySeconds / 86400f * 360f;
-		float locationLongitude = -mapLocationController.Locations[mapLocationController.CurrentLocationIndex].x;
+
+		// Adjust the longitude of the map location to be 0 to 360 degrees instead of -180 to 180
+		float locationLongitude = -currentLocationCoordinates.x;
 		if (locationLongitude < 0)
 		{
 			locationLongitude += 360f;
 		}
+
+		float eclipticLongitude = Mathf.PI * 2f * now.DayOfYear / 365f;
+		float sunDeclination = Mathf.Asin(Mathf.Sin(eclipticLongitude) * Mathf.Sin(earthAxialTiltRadians));
+		float hourAngle = Mathf.Acos(-Mathf.Tan(currentLocationCoordinates.y) * Mathf.Tan(sunDeclination));
+
+		Debug.Log(hourAngle);
 
 		// NEED TO FIX MATH
 		// Find the angle offset between the locations longitude and the suns longitude
