58eb089703612bb50a69de8063dd5aef373c6c95
Frankie Alfano <q24xboss@gmail.com>
2025-09-09 00:24:31 -0400

    Added sun positioning with accurate math

--- Assignment 1/Assets/Scripts/RealtimeSunController.cs
+++ Assignment 1/Assets/Scripts/RealtimeSunController.cs
@@ -8,45 +8,69 @@ public class RealtimeSunController : MonoBehaviour
 	[SerializeField] private ArcGISCameraComponent arcGISCameraController;
 	[SerializeField] private ArcGISMapComponent arcGISMapComponent;
 
-	private float earthCircumferenceMeters = 40075f;
+	//private float earthCircumferenceMeters = 40075f;
 	private float earthAxialTiltRadians = 23.5f * Mathf.Deg2Rad;
+	private float earthRadiansPerHour = 15f * Mathf.Deg2Rad;
+	private float earthMinutesPerRadian = 4f / Mathf.Deg2Rad;
 
 	private void Update ( )
 	{
 		// Get the current UTC time and map location
-		DateTime now = DateTime.UtcNow;
-		Vector2 currentLocationCoordinates = mapLocationController.Locations[mapLocationController.CurrentLocationIndex];
+		DateTime nowLocal = DateTime.Now;
+		Vector2 coords = mapLocationController.Locations[mapLocationController.CurrentLocationIndex] * Mathf.Deg2Rad;
 
-		// Calculate the number of seconds that have elapsed within the day
-		float currentDaySeconds = 0;
-		currentDaySeconds += now.Hour * 3600f;
-		currentDaySeconds += now.Minute * 60f;
-		currentDaySeconds += now.Second;
+		// Sum up the total hours that it has been within the day as a decimal number
+		// This allows for more accurate time conversions + faster real-time updating of the sun
+		float hoursLocal = nowLocal.Hour;
+		hoursLocal += nowLocal.Minute / 60f;
+		hoursLocal += nowLocal.Second / 3600f;
+		hoursLocal += nowLocal.Millisecond / 3600000f;
+		hoursLocal = 10f;
 
-		// Calculate the current longitude of the sun
-		float sunLongitude = currentDaySeconds / 86400f * 360f;
+		// Math Resource Links:
+		// https://www.pveducation.org/pvcdrom/properties-of-sunlight/the-suns-position
+		// https://www.pveducation.org/pvcdrom/properties-of-sunlight/elevation-angle
+		// https://www.pveducation.org/pvcdrom/properties-of-sunlight/sun-position-calculator
+		// https://en.wikipedia.org/wiki/Solar_azimuth_angle
 
-		// Adjust the longitude of the map location to be 0 to 360 degrees instead of -180 to 180
-		float locationLongitude = -currentLocationCoordinates.x;
-		if (locationLongitude < 0)
+		// Calculate the local solar time based on:
+		// - current time zone
+		// - current longitude
+		// - eccentricity of Earth's orbit
+		// - Earth's axial tilt
+		float localStandardTimeMeridian = earthRadiansPerHour * (TimeZoneInfo.Local.GetUtcOffset(nowLocal).Hours);
+		float b = (Mathf.PI * 2f / 365f) * (nowLocal.DayOfYear - 81);
+		float equationOfTime = (9.87f * Mathf.Sin(b + b)) - (7.53f * Mathf.Cos(b)) - (1.5f * Mathf.Sin(b));
+		float timeCorrectionFactor = earthMinutesPerRadian * (coords.x - localStandardTimeMeridian) + equationOfTime;
+		float localSolarTime = hoursLocal + (timeCorrectionFactor / 60f);
+
+		// Calculate the hour angle of the sun, which is the conversion of local solar time to radians moved across the sky
+		float hourAngle = earthRadiansPerHour * (localSolarTime - 12);
+
+		// Calculate the declination of the sun
+		float declination = earthAxialTiltRadians * Mathf.Sin(b);
+
+		// Calculate the elevation and azimuth angles of the sun in the sky
+		float elevation = Mathf.Asin((Mathf.Sin(declination) * Mathf.Sin(coords.y)) + (Mathf.Cos(declination) * Mathf.Cos(coords.y) * Mathf.Cos(hourAngle)));
+		float azimuth = Mathf.Acos(((Mathf.Sin(declination) * Mathf.Cos(coords.y)) - (Mathf.Cos(declination) * Mathf.Sin(coords.y) * Mathf.Cos(hourAngle))) / Mathf.Cos(elevation));
+
+		// Adjust the azimuth angle due to the fact that I am using trigonometry functions with multiple solutions (like arcsin)
+		if (localSolarTime > 12f)
 		{
-			locationLongitude += 360f;
+			azimuth = (Mathf.PI * 2f) - azimuth;
 		}
 
-		float eclipticLongitude = Mathf.PI * 2f * now.DayOfYear / 365f;
-		float sunDeclination = Mathf.Asin(Mathf.Sin(eclipticLongitude) * Mathf.Sin(earthAxialTiltRadians));
-		float hourAngle = Mathf.Acos(-Mathf.Tan(currentLocationCoordinates.y) * Mathf.Tan(sunDeclination));
+		// Set the rotation of the directional light to match the sun's position in the sky
+		transform.rotation = Quaternion.Euler(elevation * Mathf.Rad2Deg, azimuth * Mathf.Rad2Deg, 0f);
 
-		Debug.Log(hourAngle);
-
-		// NEED TO FIX MATH
-		// Find the angle offset between the locations longitude and the suns longitude
-		// 90 - locationLongitude should give the rotation of the skybox at 0 degrees longitude
-		// Adding the sunLongitude should give the current position of the sun in the sky
-		// But this does not work, something is wrong with the math
-		float sunRotation = 90 - locationLongitude + sunLongitude;
-
-		// Set the rotation of the directional light to match the sun
-		transform.rotation = Quaternion.Euler(sunRotation, 0f, 0f);
+		//Debug.Log("---------------------------------------------------------");
+		//Debug.Log($"Local Standard Time Meridan: {localStandardTimeMeridian * Mathf.Rad2Deg} degrees");
+		//Debug.Log($"Equation Of Time: {equationOfTime} minutes");
+		//Debug.Log($"Time Correction Factor: {timeCorrectionFactor} minutes");
+		//Debug.Log($"Hour Angle: {hourAngle * Mathf.Rad2Deg} degrees");
+		//Debug.Log($"Declination: {declination * Mathf.Rad2Deg} degrees");
+		//Debug.Log($"Local Solar Time: {(int) localSolarTime}:{(localSolarTime - (int) localSolarTime) * 60f:00}");
+		//Debug.Log($"Elevation: {elevation * Mathf.Rad2Deg} degrees");
+		//Debug.Log($"Azimuth: {azimuth * Mathf.Rad2Deg} degrees");
 	}
 }
