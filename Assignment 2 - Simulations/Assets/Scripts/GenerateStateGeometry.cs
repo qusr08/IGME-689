4d6669518472ed8a809222768c10abec857d9337
Frankie Alfano <q24xboss@gmail.com>
2025-09-20 02:00:26 -0400

    Added state 2D mesh generation

--- Assignment 2 - Simulations/Assets/Scripts/GenerateStateGeometry.cs
+++ Assignment 2 - Simulations/Assets/Scripts/GenerateStateGeometry.cs
@@ -1,7 +1,11 @@
+using Esri.ArcGISMapsSDK.Components;
+using Esri.GameEngine.Geometry;
+using Esri.HPFramework;
 using SimpleJSON;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Unity.Mathematics;
 using UnityEngine;
 
 public enum StateDataIndex
@@ -16,6 +20,12 @@ public enum RainfallDataIndex
 
 public class GenerateStateGeometry : MonoBehaviour
 {
+	[SerializeField] private ArcGISMapComponent arcGISMapComponent;
+	[Space]
+	[SerializeField] private Material stateMaterial;
+	[SerializeField] private float stateMinAltitude;
+	[SerializeField] private float stateMeshScale;
+
 	private const int STATE_CSV_DATA_LENGTH = 22;
 	private const int RAINFALL_CSV_DATA_LENGTH = 9;
 	private Dictionary<string, StateData> stateDataList;
@@ -28,7 +38,7 @@ public class GenerateStateGeometry : MonoBehaviour
 	private void Start ( )
 	{
 		LoadStateData( );
-		CreateStateGeometry( );
+		GenerateStateMeshGroups( );
 		SetupUI( );
 	}
 
@@ -45,7 +55,9 @@ public class GenerateStateGeometry : MonoBehaviour
 			foreach (JSONNode coordinateGroupNode in coordinateNode)
 			{
 				stateData.GeoData.Add(new List<Vector2>( ));
-				foreach (JSONNode coordinatePairNode in coordinateGroupNode)
+
+				JSONNode groupNode = coordinateGroupNode.Count == 1 ? coordinateGroupNode[0] : coordinateGroupNode;
+				foreach (JSONNode coordinatePairNode in groupNode)
 				{
 					stateData.GeoData[^1].Add(new Vector2(coordinatePairNode[0], coordinatePairNode[1]));
 				}
@@ -83,13 +95,66 @@ public class GenerateStateGeometry : MonoBehaviour
 		} while (i < csvRainfallData.Length);
 	}
 
-	private void CreateStateGeometry ( )
+	private void GenerateStateMeshGroups ( )
 	{
+		foreach (string stateName in stateDataList.Keys)
+		{
+			GenerateStateMeshGroup(stateDataList[stateName]);
+		}
 
-		UpdateStateGeometry( );
+		UpdateStateMeshes( );
 	}
 
-	private void UpdateStateGeometry ( )
+	public GameObject GenerateStateMeshGroup (StateData stateData)
+	{
+		GameObject stateMeshGroup = new GameObject($"{stateData.Name} Mesh Group");
+		stateMeshGroup.transform.SetParent(transform);
+
+		List<Vector2> meshPoints = new List<Vector2>( );
+		Material meshMaterial = new Material(stateMaterial);
+		meshMaterial.color = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+
+		for (int i = 0; i < stateData.GeoData.Count; i++)
+		{
+			meshPoints.Clear( );
+
+			// Add mesh points based on state shape
+			for (int j = 0; j < stateData.GeoData[i].Count; j++)
+			{
+				// Calculate the unity coordinate of the geographic coordinate
+				Vector2 geographicCoordinate = stateData.GeoData[i][j];
+				ArcGISPoint geographicPoint = new ArcGISPoint(geographicCoordinate.x, geographicCoordinate.y, stateMinAltitude, ArcGISSpatialReference.WGS84( ));
+				double3 universeCoordinate = arcGISMapComponent.View.GeographicToWorld(geographicPoint);
+				double3 unityCoordinate = arcGISMapComponent.GetComponent<HPRoot>( ).TransformPoint(universeCoordinate);
+
+				// Scale the mesh points down so the polygon collider component can generate a new mesh
+				//testvertices.Add(new Vector3((float) unityCoordinate[0], (float) unityCoordinate[1], (float) unityCoordinate[2]));
+				meshPoints.Add(new Vector2((float) unityCoordinate[0] / stateMeshScale, (float) unityCoordinate[2] / stateMeshScale));
+			}
+
+			// Create a gameobject to display the mesh
+			GameObject meshObject = new GameObject($"Mesh {i}");
+
+			// Create polygon collider component to generate a triangulated mesh
+			PolygonCollider2D collider2D = meshObject.AddComponent<PolygonCollider2D>( );
+			collider2D.SetPath(0, meshPoints.ToArray( ));
+			Mesh mesh = collider2D.CreateMesh(true, true);
+			collider2D.enabled = false;
+			meshObject.AddComponent<MeshFilter>( ).mesh = mesh;
+
+			// Set the material of the state
+			meshObject.AddComponent<MeshRenderer>( ).material = meshMaterial;
+
+			// Set the position of the mesh so it aligns with the map
+			meshObject.transform.SetPositionAndRotation(new Vector3(0f, stateMinAltitude, 0f), Quaternion.Euler(90f, 0f, 0f));
+			meshObject.transform.localScale = new Vector3(stateMeshScale, stateMeshScale, 1f);
+			meshObject.transform.SetParent(stateMeshGroup.transform);
+		}
+
+		return stateMeshGroup;
+	}
+
+	private void UpdateStateMeshes ( )
 	{
 
 	}
