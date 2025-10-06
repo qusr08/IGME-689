ae17b755780cb5d4fe826d01052a2049c1f2d27d
Frankie Alfano <q24xboss@gmail.com>
2025-10-05 22:35:21 -0400

    Improved car controller + smoothed track

--- Assignment 3 - Racing Game/Assets/Scripts/Car.cs
+++ Assignment 3 - Racing Game/Assets/Scripts/Car.cs
@@ -1,73 +1,109 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    [SerializeField] private Rigidbody rigidBody;
-    [SerializeField] private RacingFeatureLayerComponent racingFeatureLayerComponent;
-    [Space]
-    [SerializeField] private float acceleration;
-    [SerializeField] private float brakeDeceleration;
-    [SerializeField] private float turnSpeed;
-    [SerializeField] private bool locked;
+	[SerializeField] private Rigidbody rigidBody;
+	[SerializeField] private RacingFeatureLayerComponent racingFeatureLayerComponent;
+	[Space]
+	[SerializeField] private List<AxleInfo> axleInfos;
+	[SerializeField] private float maxMotorTorque;
+	[SerializeField] private float maxBrakeTorque;
+	[SerializeField] private float maxSteeringAngle;
+	[SerializeField] private bool locked;
 
-    private void Update()
-    {
-        // Prevent the car from gaining gravity speed when it is locked
-        rigidBody.maxLinearVelocity = locked ? 0f : 80f;
+	private void Update ( )
+	{
+		// Prevent the car from gaining gravity speed when it is locked
+		rigidBody.maxLinearVelocity = locked ? 0f : 9999f;
 
-        // When the race has not started, set the position and rotation of the car
-        if (!locked)
-        {
-            return;
-        }
+		// When the race has not started, set the position and rotation of the car
+		if (!locked)
+		{
+			return;
+		}
 
-        // Set the car's location above the track
-        int index1 = racingFeatureLayerComponent.SplineContainer.Splines[0].Count - 2;
-        int index2 = racingFeatureLayerComponent.SplineContainer.Splines[0].Count - 3;
-        Vector3 position = racingFeatureLayerComponent.GetSplineKnotMidpoint(index1, index2);
-        Quaternion rotation = racingFeatureLayerComponent.GetSplineKnotRotation(index1, index2);
-        transform.SetPositionAndRotation(position + new Vector3(0f, 8f, 0f), rotation);
-    }
+		int index1 = racingFeatureLayerComponent.SplineContainer.Splines[0].Count - 2;
+		int index2 = racingFeatureLayerComponent.SplineContainer.Splines[0].Count - 3;
+		Vector3 position = racingFeatureLayerComponent.GetSplineKnotMidpoint(index1, index2);
+		Quaternion rotation = racingFeatureLayerComponent.GetSplineKnotRotation(index1, index2);
+		transform.SetPositionAndRotation(position + new Vector3(0f, 8f, 0f), rotation);
+	}
 
-    private void FixedUpdate()
-    {
-        if (locked)
-        {
-            return;
-        }
+	private void FixedUpdate ( )
+	{
+		if (locked)
+		{
+			return;
+		}
 
-        // This is stupid and for some reason the car gets stuck in the ground if the center of mass doesn't change
-        // I think it might have something to do with the wheel colliders
-        rigidBody.centerOfMass = rigidBody.linearVelocity.magnitude > 0.1 ? Vector3.zero : Vector3.up;
+		foreach (AxleInfo axleInfo in axleInfos)
+		{
+			float verticalAxis = Input.GetAxis("Vertical");
+			float horizontalAxis = Input.GetAxis("Horizontal");
 
-        // Move the car forward/backwards with force
-        if (Input.GetKey(KeyCode.W))
-        {
-            rigidBody.AddForce(acceleration * transform.forward, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            rigidBody.AddForce(brakeDeceleration * -transform.forward, ForceMode.Impulse);
-        }
+			if (axleInfo.IsSteering)
+			{
+				axleInfo.LeftWheel.SteerAngleSmoothDamp = maxSteeringAngle * horizontalAxis;
+				axleInfo.RightWheel.SteerAngleSmoothDamp = maxSteeringAngle * horizontalAxis;
+			}
 
-        // Turn the car using torque
-        int direction = Vector3.Dot(rigidBody.linearVelocity, transform.forward) > 0 ? 1 : -1;
-        if (Input.GetKey(KeyCode.A))
-        {
-            rigidBody.AddTorque(-turnSpeed * direction * transform.up, ForceMode.Impulse);
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            rigidBody.AddTorque(turnSpeed * direction * transform.up, ForceMode.Impulse);
-        }
-    }
+			if (axleInfo.IsMotor)
+			{
+				axleInfo.LeftWheel.MotorTorque = maxMotorTorque * Mathf.Max(0, verticalAxis);
+				axleInfo.RightWheel.MotorTorque = maxMotorTorque * Mathf.Max(0, verticalAxis);
+			}
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        // The player loses when the car falls off the track and hits the ground
-        if (collision.gameObject.name.Contains("ArcGISGameObject"))
-        {
-            Debug.Log("LOSE");
-        }
-    }
+			axleInfo.LeftWheel.BrakeTorque = maxBrakeTorque * -Mathf.Min(0, verticalAxis);
+			axleInfo.RightWheel.BrakeTorque = maxBrakeTorque * -Mathf.Min(0, verticalAxis);
+
+			axleInfo.LeftWheel.UpdateMeshTransform( );
+			axleInfo.RightWheel.UpdateMeshTransform( );
+		}
+	}
+
+	private void OnCollisionEnter (Collision collision)
+	{
+		// The player loses when the car falls off the track and hits the ground
+		if (collision.gameObject.name.Contains("ArcGISGameObject"))
+		{
+			Debug.Log("LOSE");
+		}
+	}
+
+	private float Remap (float value, float from1, float to1, float from2, float to2)
+	{
+		return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+	}
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+	public Wheel LeftWheel;
+	public Wheel RightWheel;
+	public bool IsMotor;
+	public bool IsSteering;
+}
+
+[System.Serializable]
+public class Wheel
+{
+	public WheelCollider Collider;
+	public Transform MeshTransform;
+	[Range(0f, 1f)] public float SteerSmoothingSpeed;
+
+	public float SteerAngle { get => Collider.steerAngle; set => Collider.steerAngle = value; }
+	public float SteerAngleSmoothDamp { set => SteerAngle = Mathf.SmoothDampAngle(SteerAngle, value, ref steerAngleVelocity, SteerSmoothingSpeed); }
+	public float MotorTorque { get => Collider.motorTorque; set => Collider.motorTorque = value; }
+	public float BrakeTorque { get => Collider.brakeTorque; set => Collider.brakeTorque = value; }
+	public float Radius => Collider.radius;
+
+	private float steerAngleVelocity;
+
+	public void UpdateMeshTransform ( )
+	{
+		Collider.GetWorldPose(out Vector3 position, out Quaternion rotation);
+		MeshTransform.SetPositionAndRotation(position, rotation);
+	}
 }
--- Assignment 3 - Racing Game/Assets/Scripts/RacingFeatureLayerComponent.cs
+++ Assignment 3 - Racing Game/Assets/Scripts/RacingFeatureLayerComponent.cs
@@ -17,200 +17,216 @@ using UnityEngine.Splines;
 
 public class RacingFeatureLayerComponent : MonoBehaviour
 {
-    [System.Serializable]
-    public struct QueryLink
-    {
-        public string Link;
-        public string[] RequestHeaders;
-    }
+	[System.Serializable]
+	public struct QueryLink
+	{
+		public string Link;
+		public string[ ] RequestHeaders;
+	}
 
-    [System.Serializable]
-    public class GeometryData
-    {
-        public double Latitude;
-        public double Longitude;
-    }
+	[System.Serializable]
+	public class GeometryData
+	{
+		public double Latitude;
+		public double Longitude;
+	}
 
-    [System.Serializable]
-    public class PropertyData
-    {
-        public List<string> PropertyNames = new List<string>();
-        public List<string> Data = new List<string>();
-    }
+	[System.Serializable]
+	public class PropertyData
+	{
+		public List<string> PropertyNames = new List<string>( );
+		public List<string> Data = new List<string>( );
+	}
 
-    [System.Serializable]
-    public class FeatureQueryData
-    {
-        public GeometryData Geometry = new GeometryData();
-        public PropertyData Properties = new PropertyData();
-    }
+	[System.Serializable]
+	public class FeatureQueryData
+	{
+		public GeometryData Geometry = new GeometryData( );
+		public PropertyData Properties = new PropertyData( );
+	}
 
-    private List<FeatureQueryData> Features = new List<FeatureQueryData>();
-    private FeatureData featureInfo;
-    [SerializeField] private GameObject featurePrefab;
-    private JToken[] jFeatures;
+	private List<FeatureQueryData> Features = new List<FeatureQueryData>( );
+	private FeatureData featureInfo;
+	[SerializeField] private GameObject featurePrefab;
+	private JToken[ ] jFeatures;
 
-    public List<GameObject> FeatureItems = new List<GameObject>();
-    public QueryLink WebLink;
-    [SerializeField] private List<int> exclusiveFeatureIDs;
-    public SplineContainer SplineContainer;
-    private ArcGISMapComponent mapComponent;
+	public List<GameObject> FeatureItems = new List<GameObject>( );
+	public QueryLink WebLink;
+	[SerializeField] private List<int> exclusiveFeatureIDs;
+	public SplineContainer SplineContainer;
+	private ArcGISMapComponent mapComponent;
 
-    [Space]
-    [SerializeField] private GameObject checkpointPrefab;
-    [SerializeField] private Transform checkpointParent;
-    [SerializeField] private float maxCheckpointDistance;
-    private List<Checkpoint> checkpointList;
+	[Space]
+	[SerializeField] private GameObject checkpointPrefab;
+	[SerializeField] private Transform checkpointParent;
+	[SerializeField] private float maxCheckpointDistance;
+	[SerializeField] private float minCheckpointDistance;
+	private List<Checkpoint> checkpointList;
+	private List<Vector2> checkpointPositions;
 
-    private void Start()
-    {
-        checkpointList = new List<Checkpoint>();
-        mapComponent = FindFirstObjectByType<ArcGISMapComponent>();
-        StartCoroutine(nameof(GetFeatures));
-    }
+	private void Start ( )
+	{
+		checkpointList = new List<Checkpoint>( );
+		checkpointPositions = new List<Vector2>( );
+		mapComponent = FindFirstObjectByType<ArcGISMapComponent>( );
+		StartCoroutine(nameof(GetFeatures));
+	}
 
-    public void CreateLink(string link)
-    {
-        if (link != null)
-        {
-            foreach (string header in WebLink.RequestHeaders)
-            {
-                if (!link.ToLower().Contains(header))
-                {
-                    link += header;
-                }
-            }
+	public void CreateLink (string link)
+	{
+		if (link != null)
+		{
+			foreach (string header in WebLink.RequestHeaders)
+			{
+				if (!link.ToLower( ).Contains(header))
+				{
+					link += header;
+				}
+			}
 
-            WebLink.Link = link;
-        }
-    }
+			WebLink.Link = link;
+		}
+	}
 
-    public IEnumerator GetFeatures()
-    {
-        // To learn more about the Feature Layer rest API and all the things that are possible checkout
-        // https://developers.arcgis.com/rest/services-reference/enterprise/query-feature-service-layer-.htm
+	public IEnumerator GetFeatures ( )
+	{
+		// To learn more about the Feature Layer rest API and all the things that are possible checkout
+		// https://developers.arcgis.com/rest/services-reference/enterprise/query-feature-service-layer-.htm
 
-        UnityWebRequest Request = UnityWebRequest.Get(WebLink.Link);
-        yield return Request.SendWebRequest();
+		UnityWebRequest Request = UnityWebRequest.Get(WebLink.Link);
+		yield return Request.SendWebRequest( );
 
-        if (Request.result == UnityWebRequest.Result.Success)
-        {
-            CreateGameObjectsFromResponse(Request.downloadHandler.text);
-        }
-        else
-        {
-            Debug.Log(Request.result);
-        }
-    }
+		if (Request.result == UnityWebRequest.Result.Success)
+		{
+			CreateGameObjectsFromResponse(Request.downloadHandler.text);
+		}
+		else
+		{
+			Debug.Log(Request.result);
+		}
+	}
 
-    private void CreateGameObjectsFromResponse(string response)
-    {
-        // Deserialize the JSON response from the query.
-        JObject jObject = JObject.Parse(response);
-        jFeatures = jObject.SelectToken("features").ToArray();
-        CreateFeatures();
-    }
+	private void CreateGameObjectsFromResponse (string response)
+	{
+		// Deserialize the JSON response from the query.
+		JObject jObject = JObject.Parse(response);
+		jFeatures = jObject.SelectToken("features").ToArray( );
+		CreateFeatures( );
+	}
 
-    private void CreateFeatures()
-    {
-        foreach (JToken feature in jFeatures)
-        {
-            // Only add features with the specified IDs
-            if (!exclusiveFeatureIDs.Contains((int)feature.SelectToken("id")))
-            {
-                continue;
-            }
+	private void CreateFeatures ( )
+	{
+		foreach (JToken feature in jFeatures)
+		{
+			// Only add features with the specified IDs
+			if (!exclusiveFeatureIDs.Contains((int) feature.SelectToken("id")))
+			{
+				continue;
+			}
 
-            // Get coordinates in the Feature Service
-            JToken[] coordinates = feature.SelectToken("geometry").SelectToken("coordinates").ToArray();
+			// Get coordinates in the Feature Service
+			JToken[ ] coordinates = feature.SelectToken("geometry").SelectToken("coordinates").ToArray( );
 
-            Vector2 lastCheckpointCoordinate = Vector2.zero;
-            foreach (JToken coordinate in coordinates)
-            {
-                // Get the current coordinate of the next checkpoint
-                bool checkpointsTooFar = true;
-                coordinate.ToArray();
-                Vector2 checkpointCoordinate = new Vector2((float)Convert.ToDouble(coordinate[0]), (float)Convert.ToDouble(coordinate[1]));
+			Vector2 lastCheckpointCoordinate = Vector2.zero;
+			foreach (JToken coordinate in coordinates)
+			{
+				// Get the current coordinate of the next checkpoint
+				bool checkpointsTooFar = true;
+				coordinate.ToArray( );
+				Vector2 checkpointCoordinate = new Vector2((float) Convert.ToDouble(coordinate[0]), (float) Convert.ToDouble(coordinate[1]));
 
-                // If there are less than 2 checkpoints currently placed, then do not check to see if the checkpoints are too far
-                if (SplineContainer.Splines[0].Count < 2)
-                {
-                    CreateCheckpoint(checkpointCoordinate);
-                    lastCheckpointCoordinate = checkpointCoordinate;
+				// If there are less than 2 checkpoints currently placed, then do not check to see if the checkpoints are too far
+				if (SplineContainer.Splines[0].Count < 2)
+				{
+					CreateCheckpoint(checkpointCoordinate);
+					lastCheckpointCoordinate = checkpointCoordinate;
 
-                    continue;
-                }
+					continue;
+				}
 
-                // Keep creating intermediate checkpoints if the distance between them are too far
-                // This will make sure the track sits on the surface of the terrain better
-                while (checkpointsTooFar)
-                {
-                    // Get the direction of the new checkpoint
-                    Vector2 checkpointDirection = checkpointCoordinate - lastCheckpointCoordinate;
+				// Keep creating intermediate checkpoints if the distance between them are too far
+				// This will make sure the track sits on the surface of the terrain better
+				while (checkpointsTooFar)
+				{
+					// Get the direction of the new checkpoint
+					Vector2 checkpointDirection = checkpointCoordinate - lastCheckpointCoordinate;
 
-                    // If the distance to the new checkpoint is larger than the max checkpoint distance, then increase the checkpoint position by the max distance
-                    // This will gradually place checkpoints along a line until the current checkpoint coordinate is close enough to the last checkpoint coordinate
-                    // This ensures that no checkpoints are too far apart
-                    if (checkpointDirection.magnitude <= maxCheckpointDistance)
-                    {
-                        checkpointsTooFar = false;
-                        lastCheckpointCoordinate = checkpointCoordinate;
-                    }
-                    else
-                    {
-                        lastCheckpointCoordinate += checkpointDirection.normalized * maxCheckpointDistance;
-                    }
+					// If the distance to the new checkpoint is larger than the max checkpoint distance, then increase the checkpoint position by the max distance
+					// This will gradually place checkpoints along a line until the current checkpoint coordinate is close enough to the last checkpoint coordinate
+					// This ensures that no checkpoints are too far apart
+					if (checkpointDirection.magnitude <= maxCheckpointDistance)
+					{
+						checkpointsTooFar = false;
+						lastCheckpointCoordinate = checkpointCoordinate;
+					}
+					else
+					{
+						lastCheckpointCoordinate += checkpointDirection.normalized * maxCheckpointDistance;
+					}
 
-                    CreateCheckpoint(lastCheckpointCoordinate);
-                }
-            }
-        }
-    }
+					CreateCheckpoint(lastCheckpointCoordinate);
+				}
+			}
+		}
 
-    private void CreateCheckpoint(Vector2 position)
-    {
-        Checkpoint checkpoint = Instantiate(checkpointPrefab, checkpointParent).GetComponent<Checkpoint>();
-        checkpoint.Initialize(this, SplineContainer, mapComponent, new ArcGISPoint(position.x, position.y, new ArcGISSpatialReference(4326)));
-        checkpointList.Add(checkpoint);
-    }
+		// Make sure the road is smooth
+		SplineContainer.Splines[0].SetTangentMode(TangentMode.AutoSmooth);
+	}
 
-    public Vector3 GetSplineKnotMidpoint (int index1, int index2)
-    {
-        int knotCount = SplineContainer.Splines[0].Count;
+	private void CreateCheckpoint (Vector2 position)
+	{
+		// Make sure two checkpoints do not spawn on top of each other
+		foreach (Vector2 checkpointPosition in checkpointPositions)
+		{
+			if ((checkpointPosition - position).magnitude < minCheckpointDistance)
+			{
+				return;
+			}
+		}
 
-        if (knotCount < 2)
-        {
-            return Vector3.zero;
-        }
+		Checkpoint checkpoint = Instantiate(checkpointPrefab, checkpointParent).GetComponent<Checkpoint>( );
+		checkpoint.Initialize(this, SplineContainer, mapComponent, new ArcGISPoint(position.x, position.y, new ArcGISSpatialReference(4326)));
+		checkpointList.Add(checkpoint);
+		checkpointPositions.Add(position);
+	}
 
-        // The last position in the spline is the same as the start for the dataset being used
-        // This gets around that and skips the last index
-        index1 %= knotCount - 1;
-        index2 %= knotCount - 1;
+	public Vector3 GetSplineKnotMidpoint (int index1, int index2)
+	{
+		int knotCount = SplineContainer.Splines[0].Count;
 
-        Vector3 position1 = SplineContainer.Splines[0][index1].Position;
-        Vector3 position2 = SplineContainer.Splines[0][index2].Position;
-        return (position1 + position2) * 0.5f;
-    }
+		if (knotCount < 2)
+		{
+			return Vector3.zero;
+		}
 
-    public Quaternion GetSplineKnotRotation(int index1, int index2)
-    {
-        int knotCount = SplineContainer.Splines[0].Count;
+		// The last position in the spline is the same as the start for the dataset being used
+		// This gets around that and skips the last index
+		index1 %= knotCount - 1;
+		index2 %= knotCount - 1;
 
-        if (knotCount < 2)
-        {
-            return Quaternion.identity;
-        }
+		Vector3 position1 = SplineContainer.Splines[0][index1].Position;
+		Vector3 position2 = SplineContainer.Splines[0][index2].Position;
+		return (position1 + position2) * 0.5f;
+	}
 
-        // The last position in the spline is the same as the start for the dataset being used
-        // This gets around that and skips the last index
-        index1 %= knotCount - 1;
-        index2 %= knotCount - 1;
+	public Quaternion GetSplineKnotRotation (int index1, int index2)
+	{
+		int knotCount = SplineContainer.Splines[0].Count;
 
-        Vector3 position1 = SplineContainer.Splines[0][index1].Position;
-        Vector3 position2 = SplineContainer.Splines[0][index2].Position;
-        Vector3 direction = position1 - position2;
-        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        return Quaternion.Euler(0f, angle, 0f);
-    }
+		if (knotCount < 2)
+		{
+			return Quaternion.identity;
+		}
+
+		// The last position in the spline is the same as the start for the dataset being used
+		// This gets around that and skips the last index
+		index1 %= knotCount - 1;
+		index2 %= knotCount - 1;
+
+		Vector3 position1 = SplineContainer.Splines[0][index1].Position;
+		Vector3 position2 = SplineContainer.Splines[0][index2].Position;
+		Vector3 direction = position1 - position2;
+		float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+		return Quaternion.Euler(0f, angle, 0f);
+	}
 }
