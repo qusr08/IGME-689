ed3d4ed8a1bfcf834f27bf9d7192a05d1d75394a
Frankie Alfano <q24xboss@gmail.com>
2025-10-25 01:16:55 -0400

    Added UI states

--- Midterm Project - Mini Skyway/Assets/Scripts/UIManager.cs
+++ Midterm Project - Mini Skyway/Assets/Scripts/UIManager.cs
@@ -1,16 +1,41 @@
+using TMPro;
 using UnityEngine;
+
+public enum UIState
+{
+	MAIN_MENU, GAME, GAME_OVER
+}
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[SerializeField] private AirportManager airportManager;
+	[Space]
+	[SerializeField] private GameObject mainMenuContainer;
+	[Space]
+	[SerializeField] private GameObject gameContainer;
+	[SerializeField] private TextMeshProUGUI tripCounterText;
+	[Space]
+	[SerializeField] private GameObject gameOverContainer;
+	[SerializeField] private TextMeshProUGUI tripStatText;
+	[SerializeField] private TextMeshProUGUI timeStatText;
+	[SerializeField] private TextMeshProUGUI airportsStatText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public UIState State
+	{
+		get => _uiState;
+		set
+		{
+			_uiState = value;
+			mainMenuContainer.SetActive(_uiState == UIState.MAIN_MENU);
+			gameContainer.SetActive(_uiState == UIState.GAME);
+			gameOverContainer.SetActive(_uiState == UIState.GAME_OVER);
+		}
+	}
+
+	private UIState _uiState;
+
+	private void Start()
+	{
+		State = UIState.MAIN_MENU;
+	}
 }
